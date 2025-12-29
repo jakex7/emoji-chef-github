3d41ef45cdce62ba090a2b1d5acdaae97f61d5a0
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-12-29 02:30:07 +0100

    use https

--- base_url.php
+++ base_url.php
@@ -1,7 +1,9 @@
 <?php
 // define('BASE_URL', 'http://localhost:8000/');
-define('BASE_URL', 'http://frog01.mikr.us:20169/');
+// define('BASE_URL', 'http://frog01.mikr.us:20169/');
+define('BASE_URL', 'https://frog01-20169.wykr.es/');
 // define('GITHUB_URL', 'http://localhost:8000/');
 // define('GITHUB_URL', 'http://frog01.mikr.us:20169/');
+// define('GITHUB_URL', 'https://frog01-20169.wykr.es/');
 define('GITHUB_URL', 'https://github.com/jakex7/emoji-chef-github/');
 ?>
